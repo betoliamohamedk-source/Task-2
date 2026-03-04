6acf2ef81db98c2d6643593cf60958945f5e9b8b
Betolia <your_real_email@gmail.com>
2026-03-04 02:14:27 +0200

    finish

---
+++ bin/task2.dart
@@ -0,0 +1,14 @@
+import 'package:task2/task2.dart';
+import 'dart:ffi';
+
+void main() {
+  PrintYear year = PrintYear();
+
+  PrintEvenNum EvenNum = PrintEvenNum();
+  PrintStar star = PrintStar();
+  SumEvenNum sumEvenNum = SumEvenNum();
+  PrintNames names = PrintNames();
+  FactOddNumbers OddNumbers = FactOddNumbers();
+  CountDown countDown = CountDown();
+  CountUp countUp = CountUp();
+}
